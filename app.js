0eac910141a0816bb041b3a748ce40fff9a3f898
Bogdan <skkitecoolfun@gmail.com>
2021-07-21 00:14:32 +0100

    features

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-
     var $input = $('.main-input');
-
+    var $action = $('.action-buttons');
 
     $input.on('keydown', function (event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
@@ -11,11 +10,18 @@ $(document).ready(function () {
             $('.app-list-wrapper').prepend(`<div class="app-list"><span>${$inputText}</span></div>`);
 
             $('.app-list').prepend(`<button class="close-tab">X</button>`);
+            $action.addClass('display')
             $input.val("");
         }
     })
-    var $closeBtn = $('.close-tab');
-    console.log($closeBtn)
+
+    $input.on('focus', function () {
+        $(this).attr('placeholder', 'Curently typing.')
+    })
+
+    $input.on('focusout', function () {
+        $(this).attr('placeholder', 'Create a new todo...')
+    })
 
     $('.app-list-wrapper').on('click', '.close-tab', function () {
         console.log('mor')
