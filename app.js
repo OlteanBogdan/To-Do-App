5b9b027b618042927d4e2494f3e18f51093d50d2
Bogdan <skkitecoolfun@gmail.com>
2021-07-28 00:43:38 +0100

    WIP

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
 
         if (keycode === 13 && $inputText != "") {
             $('.app-list-wrapper').prepend(`<div class="app-list"><span>${$inputText}</span></div>`);
+            $('.app-list').prepend('<div class="circle-selector"></div>');
 
             $('.app-list').prepend(`<button class="close-tab">X</button>`);
             $action.addClass('display')
@@ -17,7 +18,6 @@ $(document).ready(function () {
 
             parseNumber++
             $tabNumber.text(parseNumber)
-
         }
     })
 
@@ -37,7 +37,52 @@ $(document).ready(function () {
         if ($('.app-list').length < 1) {
             $action.removeClass('display')
         }
+    })
 
+    $('.app-list-wrapper').on('click', '.circle-selector', function () {
+        $(this).toggleClass('active-checked')
+        $(this).parent().toggleClass('checked')
+    })
 
+    $('.clear-toggle').on('click', function () {
+        var checkedTabs = $('.app-list.checked').length;
+        parseNumber -= checkedTabs
+        $tabNumber.text(parseNumber)
+
+        $('.app-list.checked').remove()
+
+        if ($('.app-list').length < 1) {
+            $action.removeClass('display')
+        }
+    })
+
+    $('.completed-toggle').on('click', function () {
+        var checkedTabs = $('.app-list.checked').length;
+
+        if (checkedTabs > 0) {
+            parseNumber = checkedTabs
+            $tabNumber.text(parseNumber)
+
+            $('.app-list:not(.checked)').hide()
+            $('.app-list.checked').show()
+        }
+    })
+
+    $('.active-toggle').on('click', function () {
+        $('.app-list:not(.checked)').show()
+        $('.app-list.checked').hide()
+
+        var notCheckedTabs = $('.app-list:not(.checked)').length;
+
+        parseNumber = notCheckedTabs
+        $tabNumber.text(parseNumber)
+    })
+
+    $('.all-toggle').on('click', function () {
+        parseNumber = $('.app-list').length
+        $tabNumber.text(parseNumber)
+
+        $('.app-list:not(.checked)').show()
+        $('.app-list.checked').show()
     })
 });
